4e0de4693fa39235e6c166ac4b102aab3472c35a
AnAfterlife <2835195082@qq.com>
2019-12-07 14:34:55 +0800

    deng6

--- src/Goods/goods.js
+++ src/Goods/goods.js
@@ -5,6 +5,7 @@ import { Carousel } from 'antd';
 import { Link } from 'react-router-dom'
 import { BackTop } from 'antd';
 import { BrowserRouter, Switch } from 'react-router-dom'
+
 const { Search } = Input;
 
 var GoodsCss = require('./goods.css');
@@ -38,13 +39,13 @@ class GoodsPage extends React.Component {
                     </ul>
                 </div>
                 <div className={GoodsCss.product}>
-                    <div className={GoodsCss.product3}>
+                    <Link to='/commodity'><div className={GoodsCss.product3}>
                         <div className={GoodsCss.product2}>图片</div>
                         <div className={GoodsCss.introduce}>
                             <div className={GoodsCss.introduce2}><span>价格</span></div>
                             <div className={GoodsCss.introduce1}><span>商品介绍</span></div>
                         </div>
-                    </div>
+                    </div></Link>
                     <div className={GoodsCss.product1}>
                         <div className={GoodsCss.product2}>图片</div>
                         <div className={GoodsCss.introduce}>
