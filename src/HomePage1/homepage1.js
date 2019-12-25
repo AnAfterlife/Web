2c26258d50810630cb19b52396cd8c98d8d21c9a
helunwu <1194805422@qq.com>
2019-12-25 21:04:40 +0800

    底部

--- src/HomePage1/homepage1.js
+++ src/HomePage1/homepage1.js
@@ -5,6 +5,7 @@ import Input  from './../Input/input';
 import { Icon, Spin } from 'antd';
 import LubBoTu from './../LunBoTu/lunbotu'
 import Goods from './../Goods/goods'
+import bottomPage from '../bottom/bottom'
 import SearchPage from './Search/searchpage'
 // import {Avatar} from 'antd'
 import { List, Typography } from 'antd';
@@ -72,22 +73,23 @@ export default class homepage extends React.Component{
                 <div className={homepagecss.tu2}></div>
                 </div>
                 </div>
-                <div className={homepagecss.bottom}>
+              
+                 <div className={homepagecss.bottom}>
                 
 
                 <div className={homepagecss.bottomb}>
                     <div className={homepagecss.bottomball}>
                     <div className={homepagecss.bottomb1}><img src={require('../img/pt1.png')} width='70px' height='70px' /><a href='/'>二手市场</a> </div>
-                    <div className={homepagecss.bottomb2}><div className={homepagecss.bottombtop}><ul> <li><a>关于市场</a></li> <li>用户须知</li> <li>用户协议</li> <li>hah</li></ul> </div>  </div>
-                    <div className={homepagecss.bottomb2}><div className={homepagecss.bottombtop}> <ul> <li>关于市场</li> <li>用户须知</li> <li>用户协议</li> <li>hah</li></ul> </div> </div>
-                    <div className={homepagecss.bottomb2}><div className={homepagecss.bottombtop}><ul> <li>关于市场</li> <li>用户须知</li> <li>用户协议</li> <li>hah</li></ul> </div> </div>
-                    <div className={homepagecss.bottomb2}><div className={homepagecss.bottombtop}> <ul> <li>关于市场</li> <li>用户须知</li> <li>用户协议</li> <li>hah</li></ul> </div> </div>
-                   
+                    <div className={homepagecss.bottomb2}><div className={homepagecss.bottombtop}><ul> <li>关于市场</li> <li>关于我们</li> <li>产品介绍</li> <li>合作伙伴</li></ul> </div>  </div>
+                    <div className={homepagecss.bottomb2}><div className={homepagecss.bottombtop}> <ul> <li>帮助中心</li> <li>使用条款</li> <li>常见问题</li> <li>交流中心</li></ul> </div> </div>
+                    <div className={homepagecss.bottomb2}><div className={homepagecss.bottombtop}><ul> <li>了解更多</li> <li>授权协议</li> <li>相关政策</li> <li>友情提示</li></ul> </div> </div>
+                    <div className={homepagecss.bottomb2}><div className={homepagecss.bottombtop}> <ul> <li>客服中心</li> <li><Icon type="phone" theme="twoTone" width="20px" height="20px" />001-002-003</li> <li><div className={homepagecss.bottombtn}><a href="/merchant">在线客服</a></div></li> </ul> </div> </div>
                    
                    </div>
                 </div> 
                 
-              </div>
+              </div> 
+              
             </div>
         )
     }
