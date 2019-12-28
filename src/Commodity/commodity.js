8b1e12f0257cb4f09ec92f79197df76f83d8097e
AnAfterlife <2835195082@qq.com>
2019-12-28 15:01:07 +0800

    deng

--- src/Commodity/commodity.js
+++ src/Commodity/commodity.js
@@ -2,12 +2,20 @@ import React from 'react'
 import { Icon } from 'antd'
 import { Carousel } from 'antd';
 import LubBoTu from './../LunBoTu/lunbotu'
+import { Button, Radio} from 'antd';
+import BottomPage from '../bottom/bottom'
 var CommodityCss = require('./commodity.css')
 export default class CommodityPage extends React.Component {    
+        state = {
+          size: 'large',
+        };
+      
+        handleSizeChange = e => {
+          this.setState({ size: e.target.value });
+        };
+      
     render() {
-        function onChange(a, b, c) {
-            console.log(a, b, c);
-          }
+        const { size } = this.state;
         return (
             <div>
                 <div className={CommodityCss.header}>
@@ -22,15 +30,24 @@ export default class CommodityPage extends React.Component {
                         <div className={CommodityCss.header2}><Icon type="info-circle" />举报卖家</div>
                     </div>
                 </div>
+                <div className={CommodityCss.slideshow0}>
                 <div className={CommodityCss.slideshow}>
                     <LubBoTu></LubBoTu>
                 </div>
                 <div className={CommodityCss.introduce}>
-                <div className={CommodityCss.introduce1}>商品名称</div>
-            </div>
+                   <div className={CommodityCss.introduce1}>商品名称</div>
+                   <div className={CommodityCss.introduce2}>商品介绍</div>
+                   <div className={CommodityCss.introduce3}>服务保障</div>
+                   <div className={CommodityCss.introduce4}>商品价格</div>
+                   <div className={CommodityCss.introduce5}><Button type="primary" size={size}>我要购买</Button></div>
+                   <div className={CommodityCss.introduce5}><Button type="primary" size={size}>我要砍价</Button></div>
+                   
+                   </div>
+                   
+                </div>
+                <div className={CommodityCss.introduce6}><BottomPage/></div>
                 </div>
                 
-
                )
          }
 }
