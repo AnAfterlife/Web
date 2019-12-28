4666d376422276da321a2453656393d31163b636
PanderXr <1047693611@qq.com>
2019-12-28 19:47:44 +0800

    as

--- src/RegisterPage/RegisterPage.js
+++ src/RegisterPage/RegisterPage.js
@@ -1,24 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Button,Input } from 'antd';
-import{Link} from 'react-router-dom';
-
-
-
-
-var RegisterPagecss= require('./RegisterPage.css')
-
-
-export default class RegisterPage extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={}
-    }
-    changeValue=(e)=>{
-        this.setState({
-            [e.target.name]:e.target.value
-        })
-=======
 import { Button, Input, message } from 'antd';
 
 var RegisterPagecss= require('./RegisterPage.css')
@@ -76,7 +56,6 @@ export default class RegisterPage extends React.Component{
         xhr.setRequestHeader('content-type','application/json');
         xhr.send(JSON.stringify(data))
         
->>>>>>> 06d1d851e83c3a191ba9a6ac556ead7b28611b8e
     }
     render(){
 
@@ -84,33 +63,12 @@ export default class RegisterPage extends React.Component{
             <div className={RegisterPagecss.register}>
                 <div>
                 <div className={RegisterPagecss.registerheader}>
-<<<<<<< HEAD
-                    校园服务系统注册
-=======
 
                     购物系统注册
 
->>>>>>> 06d1d851e83c3a191ba9a6ac556ead7b28611b8e
                 </div>
                
                 <div className={RegisterPagecss.registerheader1}>
-<<<<<<< HEAD
-          
-            
-
-      
-                    <form className={RegisterPagecss.h1}>
-                        <div><label>用户名： </label><input type="text" name="username" placeholder="用户名" className={RegisterPagecss.tg} value={this.state.username} onChange={e=>this.changeValue(e)}></input></div>
-                        <div><label>密码：   </label><input type="text" name="password" placeholder="密码" className={RegisterPagecss.tg} value={this.state.pass}></input> </div>
-                        <div><label>确认密码：<input type="text" name="password" placeholder="确认密码"className={RegisterPagecss.tg}></input></label></div>
-                        <div><label>邮箱：</label><input type="text" name="email" placeholder="@qq.com"className={RegisterPagecss.tg}></input></div>
-                        <div><label>姓名：</label><input type="text" name="email" placeholder="张三"className={RegisterPagecss.tg}></input></div>
-                        <div><label>年龄：</label><Input type="text" name="email" placeholder="18"className={RegisterPagecss.tg}></Input></div>
-                        <div><label>性别：</label><input id="man" type="radio" checked="checked" name="1" />男<input id="woman" type="radio"  name="1"/>女</div>
-                        <div><p><Link to='../Homepage1'><Button className={RegisterPagecss.submit} type="button" >注册</Button></Link><span><input type="checkbox" onClick= ""></input><lable className={RegisterPagecss.checkbox}>我已阅读并同意相关条款</lable></span></p></div>
-                    </form>
-           
-=======
                 <table className={RegisterPagecss.biaoge} >
                 <p><Input type="text" name="stuID" placeholder="学号" className={RegisterPagecss.tg} value={this.state.stuID} onChange={e=>this.changeValue(e)}/></p>
                 <p><Input type="username" name="username" placeholder="姓名" className={RegisterPagecss.tg} value={this.state.username} onChange={e=>this.changeValue(e)}/></p>
@@ -123,7 +81,6 @@ export default class RegisterPage extends React.Component{
 
                 <p><Button  onClick={this.upload()}  className={RegisterPagecss.submit}>注册</Button><span><input type="checkbox" onClick= ""></input>es<lable className={RegisterPagecss.checkbox}>我已阅读并同意相关条款</lable></span></p>
                 </table>
->>>>>>> 06d1d851e83c3a191ba9a6ac556ead7b28611b8e
                 </div>
                 </div>
             </div>
