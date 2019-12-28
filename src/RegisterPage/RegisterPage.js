92fe20e7d320ef8a468abbf8dfc37cd6c945d22b
PanderXr <1047693611@qq.com>
2019-12-28 19:24:10 +0800

    更改

--- src/RegisterPage/RegisterPage.js
+++ src/RegisterPage/RegisterPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from 'antd';
+import { Button,Input } from 'antd';
 import{Link} from 'react-router-dom';
 
 
@@ -38,7 +38,7 @@ export default class RegisterPage extends React.Component{
                         <div><label>确认密码：<input type="text" name="password" placeholder="确认密码"className={RegisterPagecss.tg}></input></label></div>
                         <div><label>邮箱：</label><input type="text" name="email" placeholder="@qq.com"className={RegisterPagecss.tg}></input></div>
                         <div><label>姓名：</label><input type="text" name="email" placeholder="张三"className={RegisterPagecss.tg}></input></div>
-                        <div><label>年龄：</label><input type="text" name="email" placeholder="18"className={RegisterPagecss.tg}></input></div>
+                        <div><label>年龄：</label><Input type="text" name="email" placeholder="18"className={RegisterPagecss.tg}></Input></div>
                         <div><label>性别：</label><input id="man" type="radio" checked="checked" name="1" />男<input id="woman" type="radio"  name="1"/>女</div>
                         <div><p><Link to='../Homepage1'><Button className={RegisterPagecss.submit} type="button" >注册</Button></Link><span><input type="checkbox" onClick= ""></input><lable className={RegisterPagecss.checkbox}>我已阅读并同意相关条款</lable></span></p></div>
                     </form>
